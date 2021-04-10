31639d9968bf319dcc917d647d1902ea7e1d722f
Phil Katz <pkatz@finkkatz.com>
2021-04-10 08:49:21 -0400

    changes to index.js file and addition of CSS file

--- index.js
+++ index.js
@@ -14,42 +14,7 @@ const OUTPUT_DIR = path.resolve(_dirname, "output")
 const outputPath - path.join(OUTPUT_Dir, "team.html);
 
 //create an array to hold all of our team members
-const teamMembers = [
-    {
-        type:'input',
-        name:'role',
-        message:'What is your Role?',
-    },
-    {
-        type:'input',
-        name:'id',
-        message:'What is your ID#?',
-    },
-    {
-        type:'input',
-        name:'email',
-        message:'what is your email address?',
-    },
-    {
-        type:'input',
-        name:'tel',
-        message:'what is your phone number?',
-    },
-    {
-        type:'input',
-        name:'github',
-        message:'What is your github address?',
-    },
-    {
-        type:'input',
-        name:'school',
-        message:'What school do you atttend?',
-    },
-  
-];
 
-
-}
 
 
 // init function
@@ -61,57 +26,194 @@ function init(){
     })
 }
 init();
+
 // function to create a Manager
-    //prompt user with questions needed to satisfy the input for a manager object
-    inquirer.prompt().then()
-    //.then statement
-    //create a new instance of the Manager class
-    //push the new manager object to the team members' array
-    //call the function to create the rest of the team
+function addManager() {
+    inquirer.prompt([
+        {
+            message: "What is your team manager's name?",
+            name: "name"
+        },
+        {
+            message: "What is your team manager's email address?",
+            name: "email"
+        },
 
-    //function to create the rest of the team
-    //prompt user to select which type of employee they would like to add
-    //options include engineer, intern, or an option to not add anymore team members
-    //.then statement
-    //if a type of employee was selected, call function to add that type of emploeye
-    //if the other option was selected, call the function to create the output
+        {
+            type: "number",
+            message: "What is your team manager's office number?",
+            name: "officeNumber"
+        },
+    ])
 
-    //function to add an engineer
-    //same idea as create Manager
+        .then(function (data) {
+            const name = data.name
+            const id = 1
+            const email = data.email
+            const officeNumber = data.officeNumber
+            const teamMember = new Manager(name, id, email, officeNumber)
+            finalTeamArray.push(teamMember)
+            addTeamMembers();
+        });
 
-    //function to add an intern
-    //same idea as create manager
+}
 
-    //function to create the output
+//function to prompt for adding additional team members 
+function addTeamMembers() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Would you like to add more team members?",
+            choices: ["Yes, add an engineer", "Yes, add an intern", "No, my team is complete"],
+            name: "addMemberData"
+        }
+    ])
 
-    function writeToFile(fileName, data){
-        return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+        .then(function (data) {
+
+            switch (data.addMemberData) {
+                case "Yes, add an engineer":
+                    addEngineer();
+                    break;
+
+                case "Yes, add an intern":
+                    addIntern();
+                    break;
+                case "No, my team is complete":
+                    compileTeam();
+                    break;
+            }
+        });
+}
+
+//function to addd Engineer
+function addEngineer() {
+    inquirer.prompt([
+        {
+            message: "What is this engineer's name?",
+            name: "name"
+        },
+        {
+            message: "What is this engineer's email address?",
+            name: "email"
+        },
+        {
+            message: "What is this engineer's Github profile?",
+            name: "github"
+        }
+    ])
+
+    .then(function (data) {
+        const name = data.name
+        const id = finalTeamArray.length + 1
+        const email = data.email
+        const github = data.github
+        const teamMember = new Engineer(name, id, email, github)
+        finalTeamArray.push(teamMember)
+        addTeamMembers()
+    });
+
+};
+
+
+//function to add Intern
+function addIntern() {
+    inquirer.prompt([
+        {
+            message: "What is this intern's name?",
+            name: "name"
+        },
+        {
+            message: "What is this intern's email address?",
+            name: "email"
+        },
+        {
+            message: "What is this intern's school?",
+            name: "school"
+        }
+    ])
+
+        .then(function (data) {
+            const name = data.name
+            const id = finalTeamArray.length + 1
+            const email = data.email
+            const school = data.school
+            const teamMember = new Intern(name, id, email, school)
+            finalTeamArray.push(teamMember)
+            addTeamMembers()
+        });
+
+};
+
+// function to put the team together
+function compileTeam() {
+    console.log("//////////Team Complete. Thank you.////////")
+
+    const htmlArray = []
+    const htmlBeginning = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>${finalTeamArray[0]}</title>
+    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <style>
+     ${style}
+    </style>
+</head>
+<body>
+    <div class="banner-bar">
+        <h1>${finalTeamArray[0]}</h1>
+    </div>
+    <div class="card-container">
+    `
+    htmlArray.push(htmlBeginning);
+
+    for (let i = 1; i < finalTeamArray.length; i++) {
+        let object = `
+        <div class="member-card">
+            <div class="card-top">
+                <h2>${finalTeamArray[i].name}</h2>
+                <h2>${finalTeamArray[i].title}</h2>
+            </div>
+            <div class="card-bottom">
+                <p>Employee ID: ${finalTeamArray[i].id}</p>
+                <p>Email: <a href="mailto:${finalTeamArray[i].email}">${finalTeamArray[i].email}</a>></p>
+        `
+        if (finalTeamArray[i].officeNumber) {
+            object += `
+            <p>${finalTeamArray[i].officeNumber}</p>
+            `
+        }
+        if (finalTeamArray[i].github) {
+            object += `
+            <p>GitHub: <a href="https://github.com/${finalTeamArray[i].github}">${finalTeamArray[i].github}</a></p>
+            `
+        }
+        if (finalTeamArray[i].school) {
+            object += `
+            <p>School: ${finalTeamArray[i].school}</p>
+            `
+        }
+        object += `
+        </div>
+        </div>
+        `
+        htmlArray.push(object)
+    }
+
+    const htmlEnd = `
+    </div>
+    </body>
+    </html>
+    `
+    htmlArray.push(htmlEnd);
+
+    fs.writeFile(`./generated-html/${finalTeamArray[0]}.html`, htmlArray.join(""), function (err) {
         
-    //call the function from page-template module and pass it in the team members' array and safe to a data variable
-    //use fs module to write the file -> pass in the outputPath, the data, and the "utf-8"
+    })
+}
 
-    //call the function to create a manager to start the process
-()
-    //call init
-
-
-// function Employee(name, title, id, email, tel) {
-//     this.name = name;
-//     this.title = title;
-//     this.id = id;
-//     this.email = email;
-//     this.tel = tel;
-//     this.employeeList = () => {
-//         console.log(`Employee Name: ${this.name}, Employee Job Title: ${this.title}, Employee ID No.${this.id}, Employee eMail:${this.email}, Employee Telephone:${this.tel}`);
-//     }
-// }
-
-// const jim = new Employee('Jim Mama', 'Manager', 1, 'jim@jim.com', '555-555-1212');
-// const joe = new Employee('Joe Mama', 'Director', 2, 'joe@joe.com', '555-555-1213');
-// const jane = new Employee('Jane Doe', 'Lead Programmer', 3, 'jane@jane.com', '555-555-1214');
-// const suzy = new Employee('Suzy Jones', 'Programmer', 4, 'suzy@suzy.com', '555-555-1215');
-
-// jim.employeeList();
-// joe.employeeList();
-// jane.employeeList();
-// suzy.employeeList();
+startingPrompt()
